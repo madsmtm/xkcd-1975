cdb3a1900201e14282e0c869c2a7a476d53dfa3f
Mads Marquart <mads@marquart.dk>
2023-11-15 00:53:40 +0100

    Make the menu be properly on the menu bar

--- src/main.rs
+++ src/main.rs
@@ -17,12 +17,12 @@ use objc2::rc::{Allocated, Id};
 use objc2::runtime::ProtocolObject;
 use objc2::{declare_class, extern_methods, msg_send, mutability, sel, ClassType};
 
-use xkcd_1975::{Action, ClickAction, Data, Graph, Menu, MenuId, Reaction, State};
+use xkcd_1975::{Action, ClickAction, Conditional, Data, Graph, MenuId, Reaction, State};
 
 const NAME: &str = "XKCD 1975";
 
 pub struct DelegateState {
-    root: Menu,
+    main_menus: Vec<(Conditional, Id<CustomMenu>)>,
     state: RefCell<State>,
     graph: Graph,
 }
@@ -43,13 +43,34 @@ declare_class!(
     unsafe impl Delegate {
         #[method(init)]
         fn init(this: &Self) -> Option<&mut Self> {
+            let mtm = MainThreadMarker::new().unwrap();
             let this: Option<&mut Self> = unsafe { msg_send![super(this), init] };
             this.map(|this| {
                 let data = Data::load();
+                let main_menus = data
+                    .root
+                    .menu
+                    .entries
+                    .into_iter()
+                    .map(|entry| {
+                        let state = match &entry.reaction {
+                            Reaction::SubMenu { submenu, on_hover } => MenuState {
+                                id: submenu.id(&data.root.state),
+                                on_hover: on_hover.clone(),
+                            },
+                            _ => unreachable!(),
+                        };
+                        let menu = CustomMenu::new(mtm, state);
+                        unsafe { menu.setAutoenablesItems(false) };
+                        unsafe { menu.setDelegate(Some(ProtocolObject::from_ref(this))) };
+
+                        (entry.display, menu)
+                    })
+                    .collect();
                 Ivar::write(
                     &mut this.state,
                     Box::new(DelegateState {
-                        root: data.root.menu,
+                        main_menus,
                         state: RefCell::new(data.root.state),
                         graph: data.graph,
                     }),
@@ -65,19 +86,20 @@ declare_class!(
         #[method(applicationDidFinishLaunching:)]
         unsafe fn applicationDidFinishLaunching(&self, _: &NSNotification) {
             let mtm = MainThreadMarker::new().unwrap();
-            let app = NSApp.unwrap();
+            let app = unsafe { NSApp.unwrap() };
 
-            let menu = CustomMenu::new(
-                mtm,
-                MenuState {
-                    id: self.state.root.id.clone(),
-                    on_hover: Action::default(),
-                },
-            );
-            menu.setDelegate(Some(ProtocolObject::from_ref(self)));
-            menu.setAutoenablesItems(false);
-            self.menuNeedsUpdate(&menu);
-            app.setMainMenu(Some(&menu));
+            for (_, menu) in &self.state.main_menus {
+                self.menuNeedsUpdate(&menu);
+                let system_item = NSMenuItem::initWithTitle_action_keyEquivalent(
+                    mtm.alloc(),
+                    ns_string!(NAME),
+                    None,
+                    ns_string!(""),
+                );
+                menu.insertItem_atIndex(&system_item, 0);
+            }
+
+            self.update_main_menu();
 
             // Activate manually, since we're not being launched as an application bundle
             app.activateIgnoringOtherApps(true);
@@ -93,21 +115,30 @@ declare_class!(
 
     unsafe impl NSMenuDelegate for Delegate {
         #[method(menuWillOpen:)]
-        unsafe fn menuWillOpen(&self, menu: &NSMenu) {
+        unsafe fn _menuWillOpen(&self, menu: &NSMenu) {
             let MenuState { id, on_hover } = get_menu_state(menu);
             let data = &self.state.graph[id];
 
             let mut state = self.state.state.borrow_mut();
             state.update(on_hover);
 
-            for (item, entry) in menu.itemArray().iter().zip(&data.entries) {
+            let items = menu.itemArray();
+            let mut iter = items.iter();
+
+            // Hacky code for the main menu having the system menu
+            if menu.numberOfItems() as usize != data.entries.len() {
+                assert_eq!(menu.numberOfItems() as usize - 1, data.entries.len());
+                let _ = iter.next().unwrap();
+            }
+
+            for (item, entry) in iter.zip(&data.entries) {
                 item.setHidden(!entry.display.evaluate(&state));
                 item.setEnabled(entry.active.evaluate(&state));
             }
         }
 
         #[method(menuDidClose:)]
-        fn menuDidClose(&self, menu: &NSMenu) {
+        fn _menuDidClose(&self, menu: &NSMenu) {
             let MenuState { id, .. } = get_menu_state(menu);
             let data = &self.state.graph[id];
 
@@ -121,6 +152,7 @@ declare_class!(
             }
             let mtm = MainThreadMarker::new().unwrap();
             let MenuState { id, .. } = get_menu_state(menu);
+            dbg!(id);
             let data = &self.state.graph[id];
 
             for entry in &data.entries {
@@ -142,8 +174,8 @@ declare_class!(
                                 on_hover: on_hover.clone(),
                             },
                         );
-                        menu.setDelegate(Some(ProtocolObject::from_ref(self)));
                         menu.setAutoenablesItems(false);
+                        menu.setDelegate(Some(ProtocolObject::from_ref(self)));
                         item.setSubmenu(Some(&menu));
                     },
                     // Ignore contents; handled in `click:` instead
@@ -168,11 +200,16 @@ declare_class!(
                     unreachable!("found submenu where clickaction was expected")
                 }
                 Reaction::ClickAction { on_action, act } => {
+                    // Update state before doing the action
                     self.state.state.borrow_mut().update(&on_action);
+
                     if let Some(act) = act {
                         match act {
                             ClickAction::ColapseMenu => {
                                 // Do nothing, as the menu will close automatically after an item was clicked
+
+                                // Except we _do_ need to update the main menu...
+                                self.update_main_menu();
                             }
                             ClickAction::Nav { url } | ClickAction::Download { url, .. } => unsafe {
                                 let url = NSURL::initWithString(
@@ -194,6 +231,32 @@ declare_class!(
         }
     }
 );
+
+impl Delegate {
+    fn update_main_menu(&self) {
+        eprintln!("update main menu");
+        let app = unsafe { NSApp.unwrap() };
+
+        let menu = {
+            let state = self.state.state.borrow();
+            self.state
+                .main_menus
+                .iter()
+                .find_map(|(cond, menu)| cond.evaluate(&state).then_some(menu))
+                .expect("could not find a main menu")
+        };
+
+        unsafe {
+            menu.itemAtIndex(0)
+                .unwrap()
+                .setSubmenu(Some(&create_system_menu(app)))
+        };
+
+        unsafe { self.menuWillOpen(&menu) };
+
+        unsafe { app.setMainMenu(Some(&menu)) };
+    }
+}
 
 fn get_menu_state(menu: &NSMenu) -> &MenuState {
     assert!(menu.is_kind_of::<CustomMenu>());
@@ -241,19 +304,12 @@ impl CustomMenu {
     }
 }
 
-fn get_system_menu(app: &NSApplication) -> Id<NSMenuItem> {
+fn create_system_menu(app: &NSApplication) -> Id<NSMenu> {
     unsafe {
         let mtm = MainThreadMarker::new().unwrap();
 
         let name = ns_string!(NAME); // NSProcessInfo::processInfo().processName();
         let menu = NSMenu::initWithTitle(mtm.alloc(), &name);
-        let item = NSMenuItem::initWithTitle_action_keyEquivalent(
-            mtm.alloc(),
-            &name,
-            None,
-            ns_string!(""),
-        );
-        item.setSubmenu(Some(&menu));
 
         menu.addItemWithTitle_action_keyEquivalent(
             &NSString::from_str(&format!("About {name}")),
@@ -263,15 +319,15 @@ fn get_system_menu(app: &NSApplication) -> Id<NSMenuItem> {
         menu.addItem(&NSMenuItem::separatorItem());
 
         let services = NSMenu::initWithTitle(mtm.alloc(), ns_string!("Services"));
-        let servicesItem = NSMenuItem::initWithTitle_action_keyEquivalent(
+        let services_item = NSMenuItem::initWithTitle_action_keyEquivalent(
             mtm.alloc(),
             ns_string!("Services"),
             None,
             ns_string!(""),
         );
-        servicesItem.setSubmenu(Some(&services));
+        services_item.setSubmenu(Some(&services));
         app.setServicesMenu(Some(&services));
-        menu.addItem(&servicesItem);
+        menu.addItem(&services_item);
         menu.addItem(&NSMenuItem::separatorItem());
 
         menu.addItemWithTitle_action_keyEquivalent(
@@ -279,15 +335,15 @@ fn get_system_menu(app: &NSApplication) -> Id<NSMenuItem> {
             Some(sel!(hide:)),
             ns_string!("h"),
         );
-        let hideOthers = NSMenuItem::initWithTitle_action_keyEquivalent(
+        let hide_others = NSMenuItem::initWithTitle_action_keyEquivalent(
             mtm.alloc(),
             ns_string!("Hide Others"),
             Some(sel!(hideOtherApplications:)),
             ns_string!("h"),
         );
-        hideOthers
+        hide_others
             .setKeyEquivalentModifierMask(NSEventModifierFlagCommand | NSEventModifierFlagOption);
-        menu.addItem(&hideOthers);
+        menu.addItem(&hide_others);
         menu.addItemWithTitle_action_keyEquivalent(
             ns_string!("Show All"),
             Some(sel!(unhideAllApplications:)),
@@ -301,7 +357,7 @@ fn get_system_menu(app: &NSApplication) -> Id<NSMenuItem> {
             ns_string!("q"),
         );
 
-        item
+        menu
     }
 }
 
